100348f22edd04ac410c0008a957e9cebd3d6fc3
elliot-winch <erw331@nyu.edu>
2017-09-12 19:11:26 -0400

    Homework 1 Completed

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		TwoThreeTree tree = new TwoThreeTree();
+		
+		Scanner in = new Scanner(System.in);
+		
+		//Part 1: Insert				
+		int numInputs = in.nextInt();
+		
+		for(int i = 0; i < numInputs; i++){		
+			twothree.insert(in.next(), in.nextInt(), tree);
+		}
+		
+		//Instruction 2: Retrieve In Range
+		int numGets = in.nextInt();
+		
+		for(int i = 0; i < numGets; i++){
+			twothree.printValuesInRange(tree, in.next(), in.next());
+		}
+	}
+}
